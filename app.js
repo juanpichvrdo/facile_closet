7012c02ac725daa2e399cc3a6e4b40786ca8cead
Juan Daniel Pichardo <jpichardopena@gmail.com>
2018-09-11 20:50:25 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,69 @@
+
+
+ScrollReveal().reveal('.about__img-portrait', {
+    delay: 300,
+    duration: 500,
+    origin: 'left',
+    distance: '150%',
+});
+
+ScrollReveal().reveal('.about__img-landscape', {
+    delay: 400,
+    duration: 500,
+    origin: 'right',
+    distance: '150%',
+});
+
+ScrollReveal().reveal('.how-it-works__element-1', {
+    delay: 500,
+    duration: 500,
+    origin: 'right',
+    distance: '150%',
+});
+
+ScrollReveal().reveal('.how-it-works__element-2', {
+    delay: 600,
+    duration: 500,
+    origin: 'left',
+    distance: '150%',
+});
+
+ScrollReveal().reveal('.how-it-works__element-3', {
+    delay: 700,
+    duration: 500,
+    origin: 'right',
+    distance: '150%',
+});
+
+ScrollReveal().reveal('.features__card-1', {
+    delay: 500,
+    duration: 500,
+    origin: 'bottom',
+    distance: '150%',
+});
+ScrollReveal().reveal('.features__card-2', {
+    delay: 700,
+    duration: 500,
+    origin: 'bottom',
+    distance: '150%',
+});
+ScrollReveal().reveal('.features__card-3', {
+    delay: 900,
+    duration: 500,
+    origin: 'bottom',
+    distance: '150%',
+});
+ScrollReveal().reveal('.features__card-4', {
+    delay: 1100,
+    duration: 500,
+    origin: 'bottom',
+    distance: '150%',
+});
+
+ScrollReveal().reveal('.gallery__button', {
+    delay: 500,
+    duration: 500,
+    origin: 'right',
+    distance: '150%',
+});
+
